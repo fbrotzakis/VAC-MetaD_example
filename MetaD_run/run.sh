51aa0794900bd274cf9a24515e1c0a4bc60aa602
Faidon Brotzakis <phaedon.brotzakis@gmail.com>
2023-10-18 17:47:09 +0300

    ALA2

---
+++ MetaD_run/run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cp TOPO/plumed.dat .
+gmx_mpi grompp -f TOPO/md.mdp -c TOPO/C7eq.gro -p TOPO/topol.top -o topol.tpr -maxwarn 2
+gmx_mpi mdrun -deffnm topol -v -g --plumed plumed.dat -ntomp 1
---
+++ VAC_MetaD_analysis/run.sh
@@ -0,0 +1,1 @@
+ /Users/fdn/software/plumed_tica_test/plumed2/bin/plumed driver --noatoms --plumed plumed_tica_analysis.dat 
